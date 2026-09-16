48f79a3b89ee8bc0a746ae22289915710cbcc108
garfield678521-prog <garfield678521@gmail.com>
2026-09-16 14:57:38 +1000

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Add your Supabase project details here.
 // Keep the publishable/anon key only. Never put a service-role key in this file.
-window.SUPABASE_URL = "YOUR_SUPABASE_URL";
-window.SUPABASE_ANON_KEY = "YOUR_SUPABASE_PUBLISHABLE_OR_ANON_KEY";
+window.SUPABASE_URL = https://hanngrzdduihcgprrsvi.supabase.co ;
+window.SUPABASE_ANON_KEY = sb_publishable_ZtAuhRK3ErL_ceSATyxOsA_5nBTf0xD ;
